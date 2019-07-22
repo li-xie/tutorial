e8f527b1c8a1b5a852278e74c21584d4cefe52ee
li-xie <lixie943@gmail.com>
2019-07-22 16:40:05 -0700

    added a comment

--- CDF_max_k.m
+++ CDF_max_k.m
@@ -1,4 +1,5 @@
 function y = CDF_max_k(x, a, b, k)
+% This function calculates the CDF of the kth maximum value.
 y = zeros(size(x));
 for i = 1:k
     y = y + exp(-exp(-(x-b)/a)-(x-b)*(i-1)/a)/factorial(i-1);
